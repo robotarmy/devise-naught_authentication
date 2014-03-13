af272c71f034dcd47a5a24d74759d142c62a38a3
curtislinden <curtislinden@users.noreply.github.com>
2014-03-13 12:27:26 -0700

    Update naught_authentication.rb

--- lib/devise/naught_authentication.rb
+++ lib/devise/naught_authentication.rb
@@ -1,5 +1,6 @@
 require "devise/naught_authentication/strategy"
 Devise.add_module :naught_authentication,
+                  :model => 'devise/token_authenticatable/model',
                   :strategy => true, 
                   :controller => :sessions,
                   :route => :session
