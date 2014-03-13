964e50b3b1269136d0097fb593bad34750d06e94
curtislinden <curtislinden@users.noreply.github.com>
2014-03-13 14:58:50 -0700

    Update strategy.rb
    remove comment.

--- lib/devise/naught_authentication/strategy.rb
+++ lib/devise/naught_authentication/strategy.rb
@@ -1,11 +1,6 @@
 module Devise
   module Strategies
     class NaughtAuthentication < Devise::Strategies::Authenticatable
-
-      # This check is run before +authenticate!+ is called to determine if this
-      # authentication strategy is applicable. In this case we only try to authenticate
-      # if the login and password are present
-      #
       def valid?
         false
       end
