1fe639575c2810235359d89346bc0c00aa298d35
Curtis9 <curtis@lindenlab.com>
2014-03-20 11:59:15 -0700

    update new knowledge

--- lib/devise/naught_authentication.rb
+++ lib/devise/naught_authentication.rb
@@ -3,5 +3,5 @@ Devise.add_module :naught_authentication,
                   :model => 'devise/naught_authentication/model',
                   :strategy => true, 
                   :controller => :sessions,
-                  :route => {:session => [nil,:create,:destroy]}
+                  :route => :sessions
 
